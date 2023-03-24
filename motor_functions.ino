5c2749504b92bd1f62d07119e6692c15da5b9f71
Sophia Li <72220938+sophialiCMU@users.noreply.github.com>
2023-03-24 11:20:29 -0400

    cleaned up code

--- motor_functions.ino
+++ motor_functions.ino
@@ -21,32 +21,15 @@
 
 // called this way, it uses the default address 0x40
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
-// you can also call it with a different address you want
-//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x41);
-// you can also call it with a different address and I2C interface
-//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40, Wire);
+
+
+
 
 void setup() {
   Serial.begin(9600);
   Serial.println("16 channel PWM test!");
 
   pwm.begin();
-  /*
-   * In theory the internal oscillator (clock) is 25MHz but it really isn't
-   * that precise. You can 'calibrate' this by tweaking this number until
-   * you get the PWM update frequency you're expecting!
-   * The int.osc. for the PCA9685 chip is a range between about 23-27MHz and
-   * is used for calculating things like writeMicroseconds()
-   * Analog servos run at ~50 Hz updates, It is importaint to use an
-   * oscilloscope in setting the int.osc frequency for the I2C PCA9685 chip.
-   * 1) Attach the oscilloscope to one of the PWM signal pins and ground on
-   *    the I2C PCA9685 chip you are setting the value for.
-   * 2) Adjust setOscillatorFrequency() until the PWM update frequency is the
-   *    expected value (50Hz for most ESCs)
-   * Setting the value here is specific to each individual I2C PCA9685 chip and
-   * affects the calculations for the PWM update frequency. 
-   * Failure to correctly set the int.osc value will cause unexpected PWM results
-   */
   pwm.setOscillatorFrequency(27000000);
   pwm.setPWMFreq(1600);  // This is the maximum PWM frequency
 
@@ -57,34 +40,12 @@ void setup() {
 }
 
 int arrivingdatabyte;
-//uint8_t motors[];
-//unsigned long powers[];
-//unsigned long on_time;
 
-void loop() {
-  // Drive motor 7 at full power
-//  uint8_t pwmnum = 7;
-//  pwm.setPWM(pwmnum, 0, 4090);
-//#ifdef ESP8266
-//    yield();  // take a breather, required for ESP8266
-//#endif
-
-  // Read from serial port over and over
-  if (Serial.available() > 0) {
-    arrivingdatabyte = Serial.read(); //It will read the incoming or arriving data byte
-    Serial.print("data byte received: ");
-    Serial.println(arrivingdatabyte);
-    if (arrivingdatabyte == 97) { // 97 == "a"
-      // Try to run function
-      uint8_t motors[] = {7};
-      unsigned long powers[] = {4090};
-      unsigned long on_time = 10000;
-      Serial.println("about to run");
-      run_motors(motors, powers,on_time);
-    }
-  }
-  
-}
+struct tmpMotor{
+  unsigned long on_time;
+  uint8_t motors[16];
+  unsigned long powers[16];
+};
 
 /* Run a batch of motors at some specified powers for some time
  *  IN: motors : an array of motors to run
@@ -95,23 +56,73 @@ void loop() {
 void run_motors(uint8_t motors[], unsigned long powers[], unsigned long on_time) {
   unsigned long starttime = millis();
   unsigned long endtime = starttime;
+  // run motors for specified time
   while ((endtime - starttime) <= on_time) {
-    
+    // Run all of the motors at their specified powers
     for (int i=0; i < sizeof(motors) - 1; i++) {
-      pwm.setPWM(motors[i], 0, powers[i]);
-      Serial.print("motors, powers: ");
-      Serial.print(motors[i]);
-      Serial.print(" ");
-      Serial.println(powers[i]);
+      if (motors[i] != -1) {
+        pwm.setPWM(motors[i], 0, powers[i]); // run each motor at their given power
+        Serial.print("motors, powers: ");
+        Serial.print(motors[i]);
+        Serial.print(" ");
+        Serial.println(powers[i]);
+      }
     }
     #ifdef ESP8266 
       yield(); // take a breather, required for ESP8266
     #endif 
     endtime = millis();
   }
+  
   // turn all motors off
   Serial.println("stopping motors");
   for (int i=0; i < sizeof(motors) - 1; i++) {
+    if (motors[i] != -1) {
       pwm.setPWM(motors[i], 0, 0);
     }
+  }
+}
+
+void run_response(uint8_t wave_motors[][16], unsigned long wave_powers[][16], 
+  unsigned long on_times[], unsigned long delay_times[]) {
+ 
+  // loop through and run each wave
+  Serial.print("size of wave_motors: ");
+  Serial.println(sizeof(wave_motors));
+  for (int i=0; i < sizeof(wave_motors); i++) {
+    Serial.print("about to run wave ");
+    Serial.println(i);
+    Serial.print("wave_motors[i]: ");
+    Serial.println(wave_motors[0][0]);
+    Serial.print("wave_powers[i]: ");
+    Serial.println(wave_powers[0][0]);
+    run_motors(wave_motors[i], wave_powers[i], on_times[i]);
+
+    // delay for next wave
+    Serial.print("delaying for ");
+    Serial.print(delay_times[i]);
+    Serial.println(" seconds");
+    delay(delay_times[i]);
+}
+
+void loop() {
+  // Read from serial port over and over
+  if (Serial.available() > 0) {
+    arrivingdatabyte = Serial.read(); //It will read the incoming or arriving data byte
+    Serial.print("data byte received: ");
+    Serial.println(arrivingdatabyte);
+
+
+
+    // DO STUFF WITH INCOMING DATA
+    if (arrivingdatabyte == 97) { // 97 == "a"
+      // Try to run full response
+      uint8_t wave_motors[][16] = {{1, 4}, {2,4,5}, {3,5}};
+      unsigned long wave_powers[][16] = {{4090,4090}, {4090,4090}, {4090,4090}};
+      unsigned long on_times[] = {500, 500, 500};
+      unsigned long delay_times[] = {20, 20, 20};
+      
+      run_response(wave_motors, wave_powers, on_times, delay_times);
+    }
+  }
 }
