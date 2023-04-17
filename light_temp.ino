fd7266c5642dd76c56c55de1e0b3e29b8740628c
ae-lopez16 <114966379+ae-lopez16@users.noreply.github.com>
2023-04-16 23:11:46 -0400

    replaced delay() w millis() for timing

--- light_temp.ino
+++ light_temp.ino
@@ -13,8 +13,11 @@
 #define NUMPIXELS 60 // Number of LEDs in strip
 
 // Here's how to control the LEDs from any two pins:
-#define DATAPIN    4  //DI (blue)
-#define CLOCKPIN   5  //CI (green)
+//#define DATAPIN    4  //DI (blue)
+//#define CLOCKPIN   5  //CI (green)
+
+#define DATAPIN    12  //DI (red) 
+#define CLOCKPIN   13  //CI (orange)
 
 const int HEIGHT = 6;  //number of rows in the entire light system
 const int WIDTH = 10;  //number of cols in the entire light system
@@ -69,14 +72,25 @@ int rowIndices[HEIGHT][WIDTH] = {{0, 11, 12, 23, 24, 35, 36, 47, 48, 59},
 {4, 7, 16, 19, 28, 31, 40, 43, 52, 55}, 
 {5, 6, 17, 18, 29, 30, 41, 42, 53, 54}};
 
+//timing constants
+unsigned long previousMillis1 = 0;        // will store last time LED was updated
+unsigned long previousMillis2 = 0;        // will store last time LED was updated
+unsigned long previousMillis3 = 0;        // will store last time LED was updated
+unsigned long previousMillis4 = 0;        // will store last time LED was updated
+unsigned long previousMillis5 = 0;        // will store last time LED was updated
+unsigned long previousMillis6 = 0;        // will store last time LED was updated
+unsigned long previousMillis7 = 0;        // will store last time LED was updated
+long delayT = 750;           // milliseconds of time between cascade up and cascade down
+long delayS = 500;          // milliseconds of time between rows being turned on 
+long delayGotAKill = 250;        //ms
+long delayGotHit = 250;          //ms
+long delayLowHealthBasic = 100;  //ms
+bool ledState1 = LOW; //got a hit
+bool ledState2 = LOW; //got a kill 
+bool ledState3 = LOW; //low health
 
 
-Adafruit_DotStar strip(NUMPIXELS, DATAPIN, CLOCKPIN, DOTSTAR_BRG); //might need to change to BRG or remove 4th parameter
-// The last parameter is optional -- this is the color data order of the
-// DotStar strip, which has changed over time in different production runs.
-// Your code just uses R,G,B colors, the library then reassigns as needed.
-// Default is DOTSTAR_BRG, so change this if you have an earlier strip.
-
+Adafruit_DotStar strip(NUMPIXELS, DATAPIN, CLOCKPIN, DOTSTAR_BRG); 
 // Hardware SPI is a little faster, but must be wired to specific pins
 // (Arduino Uno = pin 11 for data, 13 for clock, other boards are different).  //test this !!!!!!!!!!!!
 //Adafruit_DotStar strip(NUMPIXELS, DOTSTAR_BRG);
@@ -106,19 +120,28 @@ void cascadeUp(uint32_t colors[], int delayS) {
   //begin w lights off
   strip.fill(offColor, 0, NUMPIXELS);
   strip.show();
-  
+  unsigned long currentMillis7 = millis();
   //set each row of lights, w a delay of delayS in between 
   for(int i = 0; i < HEIGHT; i++){
     setRow(colors[i], i); //'strip.show()' is inside this function
-    delay(delayS);
+    //delay(delayS);
+    if(currentMillis7 - previousMillis7 >= delayT){
+        previousMillis7 = currentMillis7;
+        continue; 
+     }
   }
 }
 
 //gradually turn rows of lights off (starting at the top)
-void cascadeDown(int delayS) {
+void cascadeDown(long delayS) {
+   unsigned long currentMillis6 = millis();
    for(int i = (HEIGHT-1); i > -1; i--){
     setRow(offColor, i); //'strip.show()' is inside this function
-    delay(delayS);
+    //delay(delayS);
+     if(currentMillis6 - previousMillis6 >= delayT){
+        previousMillis6 = currentMillis6;
+        continue; 
+     }
   }
 }
 
@@ -126,53 +149,110 @@ void cascadeDown(int delayS) {
 //colors[] is an array of RGB color values, color[0] is color for 0th row. color[1] is color for 1st row, and so forth 
 //DelayT is the time between the end of cascade up and the start of cascade down
 //DelayS is the time between a row turning on.
-void cascade(uint32_t colors[], int delayT, int delayS) {
+void cascade(uint32_t colors[], long delayT, long delayS) {
   cascadeUp(colors, delayS);
-  delay(delayT);
-  cascadeDown(delayS);
+  unsigned long currentMillis5 = millis();
+  if(currentMillis5 - previousMillis5 >= delayT){
+     //delay(delayT);
+     cascadeDown(delayS);
+     previousMillis5 = currentMillis5;
+  }
 }
 
 //basic low health pattern, flashes red
 void basic_LH() {
-  strip.fill(redColor, 0, NUMPIXELS);
-  strip.show();
-  delay(250);
-  strip.fill(offColor, 0, NUMPIXELS);
-  strip.show();
-  delay(150);
+  unsigned long currentMillis4 = millis();
+    if((ledState4 == HIGH) && (currentMillis4 - previousMillis4 >= delayLowHealthBasic)){
+       ledState4 = LOW;
+       previousMillis4 = currentMillis4;
+       strip.fill(offColor, 0, NUMPIXELS);
+       strip.show(); 
+    }
+    else if((ledState4 == LOW) && (currentMillis4 - previousMillis4 >= delayLowHealthBasic)){
+       ledState4 = HIGH;
+       previousMillis4 = currentMillis4;
+       strip.fill(redColor, 0, NUMPIXELS);
+       strip.show(); 
+    }  
+  
+//  strip.fill(redColor, 0, NUMPIXELS);
+//  strip.show();
+//  delay(250);
+//  strip.fill(offColor, 0, NUMPIXELS);
+//  strip.show();
+//  delay(150);
 }
 
-//medium low health patter, frequenct odf red flashes increase as healthlevel gets smaller
+//medium low health patter, frequenct of red flashes increase as healthlevel gets smaller
 void medium_LH(int healthLevel) {
+  unsigned long delayLowHealth = (unsigned long) (unsigned int) healthLevel ;
   //say low health is at a range 0-25
-  strip.fill(redColor, 0, NUMPIXELS);
-  strip.show();
-  delay(250);
-  strip.fill(offColor, 0, NUMPIXELS);
-  strip.show();
-  delay(healthLevel);
+  unsigned long currentMillis3 = millis();
+    if((ledState3 == HIGH) && (currentMillis3 - previousMillis3 >= delayLowHealth)){
+       ledState3 = LOW;
+       previousMillis3 = currentMillis3;
+       strip.fill(offColor, 0, NUMPIXELS);
+       strip.show(); 
+    }
+    else if((ledState3 == LOW) && (currentMillis3 - previousMillis3 >= delayLowHealth)){
+       ledState3 = HIGH;
+       previousMillis3 = currentMillis3;
+       strip.fill(redColor, 0, NUMPIXELS);
+       strip.show(); 
+    }  
+//  strip.fill(redColor, 0, NUMPIXELS);
+//  strip.show();
+//  delay(healthLevel);
+//  strip.fill(offColor, 0, NUMPIXELS);
+//  strip.show();
+//  delay(healthLevel);
+}
+
+void gotHitLoop(){
+    unsigned long currentMillis1 = millis();
+    if((ledState1 == HIGH) && (currentMillis1 - previousMillis1 >= delayGotHit)){
+       ledState1 = LOW;
+       previousMillis1 = currentMillis1;
+       strip.fill(offColor, 0, NUMPIXELS);
+       strip.show(); 
+    }
+    else if((ledState1 == LOW) && (currentMillis1 - previousMillis1 >= delayGotHit)){
+       ledState1 = HIGH;
+       previousMillis1 = currentMillis1;
+       strip.fill(redColor, 0, NUMPIXELS);
+       strip.show(); 
+    }  
 }
 
 void gotHit(){
-   strip.fill(redColor, 0, NUMPIXELS);
-   strip.show(); 
-   delay(100);
-   strip.fill(offColor, 0, NUMPIXELS);
-   strip.show(); 
-   delay(25);
-   trip.fill(redColor, 0, NUMPIXELS);
-   strip.show();   
+  gotHitLoop();
+  gotHitLoop(); 
+}
+
+void gotAKillLoop(){
+    unsigned long currentMillis2 = millis();
+    if((ledState2 == HIGH) && (currentMillis2 - previousMillis2 >= delayGotHit)){
+       ledState2 = LOW;
+       previousMillis2 = currentMillis2;
+       strip.fill(offColor, 0, NUMPIXELS);
+       strip.show(); 
+    }
+    else if((ledState2 == LOW) && (currentMillis2 - previousMillis2 >= delayGotHit)){
+       ledState2 = HIGH;
+       previousMillis2 = currentMillis2;
+       strip.fill(greenColor, 0, NUMPIXELS);
+       strip.show(); 
+    }     
 }
 
 void gotAKill(){
-  strip.fill(greenColor, 0, NUMPIXELS);
-   strip.show(); 
-   delay(100);
-   strip.fill(offColor, 0, NUMPIXELS);
-   strip.show(); 
-   delay(25);
-   trip.fill(greenColor, 0, NUMPIXELS);
-   strip.show();   
+  gotAKillLoop();
+  gotAKillLoop(); 
+}
+
+void defaultLights(){
+  strip.fill(purpleColor, 0, NUMPIXELS);
+  strip.show(); 
 }
 
 
@@ -181,12 +261,10 @@ void loop() {
   uint32_t colorArray[] = {pinkColor, violetColor, aquaColor, yellowGreenColor, orangeColor, redColor};  
   //uint32_t colorArray[] = {lightAquaColor, lightAquaColor, greenAquaColor, greenAquaColor, aquaColor, aquaColor};  
   //uint32_t colorArray[] = {lightGreenColor, lightGreenColor, greenAquaColor, greenAquaColor, greenColor, greenColor};  
-  cascade(colorArray, 100, 75);
+  cascade(colorArray, 100, 75); 
   strip.show(); 
-  gotHit();
-  strip.show();
-  delay(200);
-  gotAKill();
-  strip.show(); 
-  delay(200);
+  
+ 
+
+  
 }
