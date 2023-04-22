2e8a0ec3fad4b02571e946fe9f40a71dc5e44fbe
ae-lopez16 <114966379+ae-lopez16@users.noreply.github.com>
2023-04-22 17:16:04 -0400

    changed motor functions, made lights simple

--- Integration_wired.ino
+++ Integration_wired.ino
@@ -101,16 +101,15 @@ Adafruit_DotStar strip(NUMPIXELS, DATAPIN, CLOCKPIN, DOTSTAR_BRG);
 //////////////////////////// MOTOR CODE DECLARATIONS ///////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////
 
+#include <Adafruit_PWMServoDriver.h>
+
+// called this way, it uses the default address 0x40
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
 int NUMWAVES = 5;
 int NUMSYSM = 16;
 int arrivingdatabyte;
 
-const int motor13Pin = 3;
-const int motor14Pin = 5;
-const int motor7Pin = 6;
-const int motor4Pin = 9;
-const int motor2Pin = 10;
-const int motor1Pin = 11;
 
 ////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////// END OF MOTOR CODE DECLARATIONS ////////////////////////
@@ -206,16 +205,20 @@ void parse_request(String req, gameData *ga){
 
 void setup() {
   strip.begin(); // Initialize pins for output
+  //strip.fill(blueColor, 0, NUMPIXELS);
   strip.show();  // Turn all LEDs off ASAP
   Serial.begin(9600);
+
+  //motor setup
+  pwm.begin();
+  pwm.setOscillatorFrequency(27000000);
+  pwm.setPWMFreq(1600);  // This is the maximum PWM frequency
+
+  // if you want to really speed stuff up, you can go into 'fast 400khz I2C' mode
+  // some i2c devices dont like this so much so if you're sharing the bus, watch
+  // out for this!
+  Wire.setClock(400000);
   
-  //initializing pin modes for motors 
-  pinMode(motor13Pin, OUTPUT);
-  pinMode(motor14Pin, OUTPUT);
-  pinMode(motor7Pin, OUTPUT);
-  pinMode(motor4Pin, OUTPUT);
-  pinMode(motor2Pin, OUTPUT);
-  pinMode(motor1Pin, OUTPUT);
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
@@ -396,45 +399,73 @@ void defaultLights(){
  *  IN: on_time : time to keep the motors on
  *  OUT : nothing -- controls I2C vibration motors
 */
-void run_motors(uint8_t motors[], unsigned long powers[], unsigned long on_time) {
+void run_motors(int motors[], unsigned long powers[], unsigned long on_time, int num_on) {
   unsigned long starttime = millis();
   unsigned long endtime = starttime;
   int MAGICNUM = 10;
+  // PRINT OUT MOTORS
+
+  Serial.print("size of wave ");
+  Serial.print(num_on);
+  Serial.println("");
+
+
+  
+  Serial.print("\nwave_motors[i]: ");
+    for (int m = 0; m < NUMSYSM; m++) {
+      if (motors[m] != -1) {
+        Serial.print(motors[m]);
+        Serial.print(" ");
+      }
+    }
+    Serial.println("");
+    Serial.print("wave_powers[i]: ");
+    for (int m = 0; m < NUMSYSM; m++) {
+      if (powers[m] != 0) {
+        Serial.print(powers[m]);
+        Serial.print(" ");
+      }
+    }
+    Serial.println("");
+  
   // run motors for specified time
   while ((endtime - starttime) <= on_time) {
     // Run all of the motors at their specified powers
-    for (int i=0; i < NUMSYSM; i++) {
-      if (motors[i] != 0) {
-        if (motors[i] == 1) {
-          analogWrite(motor1Pin, powers[i]);
-        }if (motors[i] == 2) {
-          analogWrite(motor2Pin, powers[i]);
-        }if (motors[i] == 4) {
-          analogWrite(motor4Pin, powers[i]);
-        }if (motors[i] == 7) {
-          analogWrite(motor7Pin, powers[i]);
-        }if (motors[i] == 13) {
-          analogWrite(motor13Pin, powers[i]);
-        }if (motors[i] == 14) {
-          analogWrite(motor14Pin, powers[i]);
+    // if only one motor has to turn on, turn it on to full power
+    if (num_on < 2) {
+      for (int i=0; i < NUMSYSM; i++) {
+        if (motors[i] != -1) {
+          pwm.setPWM(motors[i], 0, powers[i]); // run each motor at their given power
+//          pwm.writeMicroseconds(motors[i], MAGICNUM);
         }
       }
+      #ifdef ESP8266 
+        yield(); // take a breather, required for ESP8266
+      #endif 
+      endtime = millis();
     }
-    #ifdef ESP8266 
-      yield(); // take a breather, required for ESP8266
-    #endif 
-    endtime = millis();
+
+    else { // if there are 2+ motors that are supposed to be on at the same time
+      for (uint16_t i=0; i<4096; i += 8) {
+        for (uint8_t pwmidx=0; pwmidx < 16; pwmidx++) {
+          if (motors[pwmidx] != -1) {
+            pwm.setPWM(motors[pwmidx], 0, (i + (4096/num_on)*motors[pwmidx]) % powers[pwmidx] );
+//            pwm.setPWM(motors[pwmidx], 0, (powers[pwmidx] + (4096/num_on)*motors[pwmidx]) % powers[pwmidx] );
+          }
+        }
+          
+        }
+      #ifdef ESP8266 
+        yield(); // take a breather, required for ESP8266
+      #endif 
+      endtime = millis();
+    }
   }
   
   // turn all motors off
   // Serial.println("stopping motors");
-  for (int i=0; i < sizeof(motors); i++) {
-    analogWrite(motor1Pin, 0);
-    analogWrite(motor2Pin, 0);
-    analogWrite(motor4Pin, 0);
-    analogWrite(motor7Pin, 0);
-    analogWrite(motor13Pin, 0);
-    analogWrite(motor14Pin, 0);
+  for (int i=0; i < NUMSYSM; i++) {
+    pwm.setPWM(i, 0, 0);
   }
 }
 
@@ -444,13 +475,34 @@ void run_motors(uint8_t motors[], unsigned long powers[], unsigned long on_time)
  *  IN: on_times    : time in miliseconds for how long to delay between waves
  *  OUT : nothing -- controls I2C vibration motors
 */
-void run_response(uint8_t wave_motors[][16], unsigned long wave_powers[][16], 
+void run_response(int wave_motors[][16], unsigned long wave_powers[][16], 
   unsigned long on_times[], unsigned long delay_times[]) {
  
   // loop through and run each wave
+  Serial.print("size of wave_motors: ");
+  Serial.println(sizeof(wave_motors));
+  int num_on; 
   for (int i=0; i < NUMWAVES; i++) {
-    run_motors(wave_motors[i], wave_powers[i], on_times[i]);
-    //Sleep(delay_times[i]);
+    Serial.print("\nabout to run wave ");
+    Serial.println(i);
+    
+    // calculate how many motors to turn on / have to be cycled through
+    num_on = 0;
+    for (int m=0; m < NUMSYSM; m++) {
+      if (wave_motors[i][m] != -1) {
+        num_on++;
+      }
+    }
+
+    if (num_on > 0) {
+      run_motors(wave_motors[i], wave_powers[i], on_times[i], num_on);
+    }
+
+    // delay for next wave
+    Serial.print("delaying for ");
+    Serial.print(delay_times[i]);
+    Serial.println(" milli seconds");
+    delay(delay_times[i]);
   }
 }
 
@@ -458,92 +510,92 @@ void run_response(uint8_t wave_motors[][16], unsigned long wave_powers[][16],
 void run_response1(int val) {
   int i = val / 100;
   // Try to run full response
-  uint8_t wave_motors[][16] = {{7,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0}, 
-                               {4,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0}, 
-                               {1,2,13,14, 0,0,0,0, 0,0,0,0, 0,0,0,0}, 
-                               {4,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0}, 
-                               {7,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0}};
-  unsigned long wave_powers[][16] = {{4090*i,4090*i,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0}, 
-                                     {4090*i,4090*i,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0}, 
-                                     {4090*i,4090*i,4090*i,4090*i, 0,0,0,0, 0,0,0,0, 0,0,0,0}, 
-                                     {4090*i,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0},
-                                     {4090*i,4090*i,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0}};
+  int o = -1;
+  int wave_motors[][16] = {{2,7,3,6, o,o,o,o, o,o,o,o, o,o,o,o}, 
+                           {1,5,o,o, o,o,o,o, o,o,o,o, o,o,o,o}, 
+                           {0,4,o,o, o,o,o,o, o,o,o,o, o,o,o,o}, 
+                           {1,5,o,o, o,o,o,o, o,o,o,o, o,o,o,o}, 
+                           {2,7,3,6, o,o,o,o, o,o,o,o, o,o,o,o}};
+  unsigned long wave_powers[][16] = {{4096*i,4096*i,4096*i,4096*i, 0,0,0,0, 0,0,0,0, 0,0,0,0}, 
+                                     {4096*i,4096*i,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0}, 
+                                     {4096*i,4096*i,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0}, 
+                                     {4096*i,4096*i,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0},
+                                     {4090*i,4090*i,4090*i,4090*i, 0,0,0,0, 0,0,0,0, 0,0,0,0}};
   unsigned long on_times[] = {500,500,750,500,500};
   unsigned long delay_times[] = {20, 20, 20, 20, 20};
   
   run_response(wave_motors, wave_powers, on_times, delay_times);
 }
 
-// Response 2 is the hit by small enemy
+// Responce 2 is the hit by small enemy
 void run_response2(int val) {
   int i = val / 100;
   // Try to run full response
-  uint8_t wave_motors[][16] = {{4,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0},
-                               {3,5,7,0, 0,0,0,0, 0,0,0,0, 0,0,0,0},
-                               {3,5,6,8, 0,0,0,0, 0,0,0,0, 0,0,0,0},
-                               {0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0},
-                               {0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0}};
-  unsigned long wave_powers[][16] = {{4090*i,0*i,0*i,0*i,             0*i,0*i,0*i,0*i, 0,0,0,0, 0,0,0,0},
-                                     {4090*i,4090*i,4090*i,0*i,       0*i,0*i,0*i,0*i, 0,0,0,0, 0,0,0,0},
-                                     {4090*i,4090*i,4090*i,4090*i,    0*i,0*i,0*i,0*i, 0,0,0,0, 0,0,0,0},
-                                     {0*i,0*i,0*i,0*i,                0*i,0*i,0*i,0*i, 0,0,0,0, 0,0,0,0},
+  int o = -1;
+  int wave_motors[][16] = {{3,6,o,o, o,o,o,o, o,o,o,o, o,o,o,o}, 
+                           {2,7,1,5, o,o,o,o, o,o,o,o, o,o,o,o}, 
+                           {o,o,o,o, o,o,o,o, o,o,o,o, o,o,o,o}, 
+                           {o,o,o,o, o,o,o,o, o,o,o,o, o,o,o,o}, 
+                           {o,o,o,o, o,o,o,o, o,o,o,o, o,o,o,o}};
+  unsigned long wave_powers[][16] = {{4090*i,0*i,0*i,0*i,             0*i,0*i,0*i,0*i, 0,0,0,0, 0,0,0,0}, 
+                                     {4090*i,4090*i,4090*i,0*i,       0*i,0*i,0*i,0*i, 0,0,0,0, 0,0,0,0}, 
+                                     {4090*i,4090*i,4090*i,4090*i,    0*i,0*i,0*i,0*i, 0,0,0,0, 0,0,0,0}, 
+                                     {0*i,0*i,0*i,0*i,                0*i,0*i,0*i,0*i, 0,0,0,0, 0,0,0,0}, 
                                      {0*i,0*i,0*i,0*i,                0*i,0*i,0*i,0*i, 0,0,0,0, 0,0,0,0}};
-  unsigned long on_times[] = {500, 500, 500, 500, 500, 0};
-  unsigned long delay_times[] = {20, 20, 20, 20, 0};
+  unsigned long on_times[] = {500, 750, 0, 0, 0, 0};
+  unsigned long delay_times[] = {20, 0, 0, 0, 0};
+  
   run_response(wave_motors, wave_powers, on_times, delay_times);
 }
-// Response 3 is the hit by large enemy
+
+// Responce 3 is the hit by large enemy
 void run_response3(int val) {
   int i = val / 100;
   // Try to run full response
-  uint8_t wave_motors[][16] = {{4,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0},
-                               {3,5,7,0, 0,0,0,0, 0,0,0,0, 0,0,0,0},
-                               {3,5,6,8, 0,0,0,0, 0,0,0,0, 0,0,0,0},
-                               {9,10,11,12, 0,0,0,0, 0,0,0,0, 0,0,0,0},
-                               {0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0}};
-  unsigned long wave_powers[][16] = {{4090*i,0*i,0*i,0*i,             0*i,0*i,0*i,0*i, 0,0,0,0, 0,0,0,0},
-                                     {4090*i,4090*i,4090*i,0*i,       0*i,0*i,0*i,0*i, 0,0,0,0, 0,0,0,0},
-                                     {4090*i,4090*i,4090*i,4090*i,    0*i,0*i,0*i,0*i, 0,0,0,0, 0,0,0,0},
-                                     {4090*i,4090*i,4090*i,4090*i,                0*i,0*i,0*i,0*i, 0,0,0,0, 0,0,0,0},
+  int o = -1;
+  int wave_motors[][16] = {{3,6,o,o, o,o,o,o, o,o,o,o, o,o,o,o}, 
+                           {2,7,1,5, o,o,o,o, o,o,o,o, o,o,o,o}, 
+                           {0,4,o,o, o,o,o,o, o,o,o,o, o,o,o,o}, 
+                           {o,o,o,o, o,o,o,o, o,o,o,o, o,o,o,o}, 
+                           {o,o,o,o, o,o,o,o, o,o,o,o, o,o,o,o}};
+  unsigned long wave_powers[][16] = {{4090*i,0*i,0*i,0*i,             0*i,0*i,0*i,0*i, 0,0,0,0, 0,0,0,0}, 
+                                     {4090*i,4090*i,4090*i,0*i,       0*i,0*i,0*i,0*i, 0,0,0,0, 0,0,0,0}, 
+                                     {4090*i,4090*i,4090*i,4090*i,    0*i,0*i,0*i,0*i, 0,0,0,0, 0,0,0,0}, 
+                                     {0*i,0*i,0*i,0*i,                0*i,0*i,0*i,0*i, 0,0,0,0, 0,0,0,0}, 
                                      {0*i,0*i,0*i,0*i,                0*i,0*i,0*i,0*i, 0,0,0,0, 0,0,0,0}};
-  unsigned long on_times[] = {500, 500, 500, 500, 500, 0};
-  unsigned long delay_times[] = {20, 20, 20, 20, 0};
+  unsigned long on_times[] = {500, 750, 250, 0, 0, 0};
+  unsigned long delay_times[] = {20, 20, 0, 0, 0};
+  
   run_response(wave_motors, wave_powers, on_times, delay_times);
 }
-// Response 4 is the low health
+
+// Responce 4 is the low health
 void run_response4(int val) {
   int i = val / 100;
   // Try to run full response
-  uint8_t wave_motors[][16] = {{2,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0},
-                               {2,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0},
-                               {2,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0},
-                               {2,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0},
-                               {2,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0}};
-  unsigned long wave_powers[][16] = {{4090*i,0*i,0*i,0*i, 0*i,0*i,0*i,0*i, 0,0,0,0, 0,0,0,0},
-                                     {4090*i,0*i,0*i,0*i, 0*i,0*i,0*i,0*i, 0,0,0,0, 0,0,0,0},
-                                     {4090*i,0*i,0*i,0*i, 0*i,0*i,0*i,0*i, 0,0,0,0, 0,0,0,0},
-                                     {4090*i,0*i,0*i,0*i, 0*i,0*i,0*i,0*i, 0,0,0,0, 0,0,0,0},
-                                     {4090*i,0*i,0*i,0*i, 0*i,0*i,0*i,0*i, 0,0,0,0, 0,0,0,0}};
-  unsigned long on_times[] = {100, 100, 100, 100, 100, 0};
-  unsigned long delay_times[] = {20, 20, 20, 20, 0};
+  int o = -1;
+  int wave_motors[][16] = {{4,o,o,o, o,o,o,o, o,o,o,o, o,o,o,o}, 
+                           {4,o,o,o, o,o,o,o, o,o,o,o, o,o,o,o}, 
+                           {4,o,o,o, o,o,o,o, o,o,o,o, o,o,o,o}, 
+                           {4,o,o,o, o,o,o,o, o,o,o,o, o,o,o,o}, 
+                           {o,o,o,o, o,o,o,o, o,o,o,o, o,o,o,o}};
+  unsigned long wave_powers[][16] = {{4090*i,0*i,0*i,0*i,             0*i,0*i,0*i,0*i, 0,0,0,0, 0,0,0,0}, 
+                                     {4090*i,4090*i,4090*i,0*i,       0*i,0*i,0*i,0*i, 0,0,0,0, 0,0,0,0}, 
+                                     {4090*i,4090*i,4090*i,4090*i,    0*i,0*i,0*i,0*i, 0,0,0,0, 0,0,0,0}, 
+                                     {0*i,0*i,0*i,0*i,                0*i,0*i,0*i,0*i, 0,0,0,0, 0,0,0,0}, 
+                                     {0*i,0*i,0*i,0*i,                0*i,0*i,0*i,0*i, 0,0,0,0, 0,0,0,0}};
+  unsigned long on_times[] = {200, 200, 200, 200, 200, 0};
+  unsigned long delay_times[] = {20, 20, 20, 20, 200};
+  
   run_response(wave_motors, wave_powers, on_times, delay_times);
 }
+
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////END OF MOTOR CODE FNS  //////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////
 
-
-//// helper-function for easy to use non-blocking timing
-//boolean TimePeriodIsOver (unsigned long &StartTime, unsigned long TimePeriod) {
-//  unsigned long currentMillis  = millis();  
-//  if ( currentMillis - StartTime >= TimePeriod ){
-//    StartTime = currentMillis; // store timestamp when the new interval has started
-//    return true;               // more time than TimePeriod) has elapsed since last time if-condition was true
-//  } 
-//  else return false; // return value false because LESS time than TimePeriod has passed by
-//}
 
 
 void eventHandler(){
@@ -554,25 +606,28 @@ void eventHandler(){
   switch(currState){
     
     case (forcefulJump):
-      //run_response1(motorIntensity); //motor response 
-      cascade(colorArray, 750, 500); //750 and 500 come from "on_times" array under motor functions
+      run_response1(motorIntensity); //motor response 
+      strip.fill(greenColor, 0, NUMPIXELS);
       strip.show(); //not sure if i need this
       break;
       
     case (hitSmall):
-      //run_response2(motorIntensity); //motor response
-      gotHitSmall(); //light response 
+      run_response2(motorIntensity); //motor response
+      strip.fill(lightGreenColor, 0, NUMPIXELS);
+      strip.show();
       break;
       
     case (hitLarge):
-       //run_response3(motorIntensity);
+       run_response3(motorIntensity);
+       strip.fill(blueColor, 0, NUMPIXELS);
+       strip.show();
        gotHitLarge();  
       break;
       
     case (lowHealth):
-       //run_response4(motorIntensity);
-       //medium_LH(healthLevel);
-       basic_LH(); 
+       run_response4(motorIntensity);
+       strip.fill(redColor, 0, NUMPIXELS);
+       strip.show(); 
       break;
      
     default: 
@@ -594,39 +649,57 @@ void loop() {
       int rlen = Serial.readBytesUntil('\n', buf, BUFFER_SIZE);
       String incomingReq = "";
       for(int i = startingIndex; i < rlen+startingIndex; i++){
-        incomingReq += char(buf[i]);  
+        incomingReq += char(buf[i]);    //eventually clear buffer 
         startingIndex = rlen;  
       }
       parse_request(incomingReq, &gameActions);
-      eventHandler(); 
+      eventHandler();  
     }
   }
 }
 
-//test out the parallelism of the actions. Looking to see if the motor/light responses even
-//register individually, and how long they last (under the milli time requirement)
+
 void runTests(){
-  int tempHealthLevel = 25; 
-  for(int i = 0; i < 5; i++){
-    Serial.print("PARELLISM TEST FOR ");
-    if(i == 0){
-      Serial.println("FORCEFUL JUMP: ");
-    } 
-    if(i == 1){
-      Serial.println("HIT SMALL: ");
+    Serial.println("run testssssss");
+    bool doFJ = true;
+    bool doHS = false;
+    bool doHL = false;
+    bool doLH = false; 
+    unsigned long currentMillis8 = millis();
+    if(currentMillis8 - previousMillis8 >= 5000){
+      if(doFJ){
+        //FJ
+        Serial.println("doing FJ response");
+        run_response1(80); //motor response
+        //cascade(colorArray, 750, 500); //750 and 500 come from "on_times" array under motor functions
+        strip.fill(blueColor, 0, NUMPIXELS);
+        doFJ = false;
+        doHS = true; 
+      }
+      if(doHS){
+        //hit small 
+        Serial.println("doing HS response");
+        run_response2(80); //motor response
+        
+        doHS = false;
+        doHL = true; 
+      }
+      if(doHL){
+        //hit large 
+        Serial.println("doing HL response");
+        run_response3(80);
+        
+        doHL = false;
+        doLH = true;
+      }
+      if(doLH){
+        //low health
+        Serial.println("doing Low health response");
+        run_response4(80);
+        
+        doLH = false;
+        doFJ = true; 
+      }
+      previousMillis8 = currentMillis8;
     }
-    if(i == 2){
-      Serial.println("HIT LARGE: ");
-    }
-    if(i == 3){
-      Serial.println("LOW HEALTH: ");
-    }
-    if(i == 4){
-      Serial.println("no action: ");
-    }
-    gameActions.action = i;
-    gameActions.healthLevel = tempHealthLevel-i; 
-    eventHandler();
-    delay(5000)
-  }
 }
